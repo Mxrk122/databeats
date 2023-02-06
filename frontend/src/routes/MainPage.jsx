7c3b6d0d839cf1f1d08d18672920b2c8f3c84b91
José Rodrigo Barrera <bar20807@uvg.edu.gt>
2023-02-05 21:30:08 -0600

    ArrayDeVinilosJSON

--- frontend/src/routes/MainPage.jsx
+++ frontend/src/routes/MainPage.jsx
@@ -1,5 +1,48 @@
 import React from 'react'
+//Creamos un main donde se pondrá el array de los vinilos con un formato json
 
-const MainPage = () => <h1>Main Page</h1>
+
+
+const MainPage = () => {
+    //Creamos un array de tipo JSON con los datos de los vinilos
+    const vinilos = [
+        {
+            "id": 1,
+            "nombre": "Abbey Road",
+            "artista": "The Beatles",
+            "precio": 20,
+            "imagen": 'https://revistaastronauta.com/wp-content/uploads/2022/04/1200px-7C396001-AA89-4EF5-8732-342FE9276B38.jpg'
+        },
+        {
+            "id": 2,
+            "nombre": "The Dark Side of the Moon",
+            "artista": "Pink Floyd",
+            "precio": 20,
+            "imagen": 'https://www.audiovenue.uk/wp-content/uploads/2021/07/pink-floyd-dark-side.jpg'
+        }
+    ]
+
+    return (
+        <main>
+        <div className="vinilos-container">
+            <div className="vinilos">
+            <h1>¡Bienvenido a Data Beats!</h1>
+            <div className = "tablero">
+                {
+                vinilos.map((vinilo) => (
+                    <div className = "vinilo">
+                        <img src = {vinilo.imagen} alt = {vinilo.nombre}/>
+                        <h2>{vinilo.nombre}</h2>
+                        <h3>{vinilo.artista}</h3>
+                        <h4>{vinilo.precio}€</h4>
+                    </div>
+                ))}
+            </div>
+            <p>Estos son los vinilos que tenemos en nuestra tienda</p>
+            </div>
+        </div>
+        </main>
+    )
+}
 
 export default MainPage
