16c4a55348d26e070e8a3a2d10744af420b7b743
Pedro Arriola <pedroparty310@gmail.com>
2023-02-12 01:40:03 -0600

    Se ha rediseñado login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { UserProvider } from './context/userContextProvider'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = document.getElementById('root')
 const reactRoot = ReactDOM.createRoot(root)
@@ -11,7 +12,9 @@ reactRoot.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
+        <ChakraProvider>
         <App />
+        </ChakraProvider>
       </UserProvider>  
     </BrowserRouter>
   </React.StrictMode>
--- frontend/src/routes/LoginPage.jsx
+++ frontend/src/routes/LoginPage.jsx
@@ -2,6 +2,17 @@ import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import DataBeatsLogo from '../assets/images/DataBeatsLogo.png'
 import { UserContext } from '../context/userContextProvider'
+import { 
+  Box, 
+  Input, 
+  Button, 
+  Heading, 
+  Text, 
+  Flex, 
+  FormControl, 
+  FormLabel, 
+  Stack 
+} from '@chakra-ui/react'
 
 const LoginPage = ({ handleFavorites }) => {
   const [users, setUsers] = useState()
@@ -40,33 +51,71 @@ const LoginPage = ({ handleFavorites }) => {
   }
 
   return (
-    <main>
-      <div className="login-container">
-        <div className="login-icon-container">
-          <img src={DataBeatsLogo} alt="Data Beats Logo" />
-          <h1>¡Bienvenido a Data Beats!</h1>
-        </div>
-        <div className="login-form">
-          <input
-            type="text"
-            id="username"
-            placeholder="Username"
-            onChange={(event) => setUsername(event.target.value)}
-          />
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button onClick={handleLogin}>Iniciar Sesión</button>
-        <p>
-          ¿Aún no posees cuenta? <Link to="/sign-up">Registrate</Link> aquí.
-        </p>
-      </div>
-    </main>
-  )
-}
+    <Flex 
+      h="100vh" 
+      w="100vw" 
+      align="center" 
+      justify="center" 
+      direction="column"
+      bgColor="#13955E"
+    >
+      <Box w={["100%", "30%"]} p={8} rounded="lg" bg="white" boxShadow="dark-lg">
+        <Stack spacing={8}>
+          <Box className="login-icon-container" align="center">
+            <img src={DataBeatsLogo} alt="Data Beats Logo" />
+            <Heading as="h1" textAlign="center">¡Bienvenido a Data Beats!</Heading>
+          </Box>
+          <FormControl>
+            <FormLabel htmlFor="username" fontSize="lg" color="gray.600">Usuario</FormLabel>
+            <Input
+              type="text"
+              id="username"
+              placeholder="Username"
+              onChange={(event) => setUsername(event.target.value)}
+              rounded="lg"
+              w="100%"
+              mt={4}
+              bg="gray.200"
+              p={4}
+              _focus={{ bg: "white", boxShadow: "outline" }}
+            />
+          </FormControl>
 
-export default LoginPage
+          <FormControl>
+            <FormLabel htmlFor="password" fontSize="lg" color="gray.600">Contraseña</FormLabel>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Password"
+              onChange={(event) => setPassword(event.target.value)}
+              rounded="lg"
+              w="100%"
+              mt={4}
+              bg="gray.200"
+              p={4}
+              _focus={{ bg: "white", boxShadow: "outline" }}
+            />
+          </FormControl>
+
+          <Button 
+            onClick={handleLogin}
+            mt={8}
+            w="100%"
+            variantColor="green"
+            variant="solid"
+            rounded="lg"
+            fontSize="lg"
+            bg="grey.200"
+            _hover={{ bg: "green.500" }}
+            _active={{ bg: "green.700" }}>Iniciar Sesión</Button>
+          
+          <Text textAlign="center" mt={8} fontSize="sm" color="gray.500">
+            ¿Aún no tienes una cuenta? <Link to="/sign-up">Regístrate</Link>
+          </Text>
+        </Stack>
+      </Box>
+    </Flex>
+  );
+};
+
+export default LoginPage;
